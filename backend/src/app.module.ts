2c328627c7c86e49b51762dcfe75569406fa9957
Kelvin Giovanno <kelvingiovanno2000@gmail.com>
2026-03-10 00:56:27 +0700

    remove hello module in app module

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,7 +2,6 @@ import { Module } from '@nestjs/common';
 import { AuthModule } from './module/auth/auth.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { ConfigModule } from '@nestjs/config';
-import { HelloModule } from './module/hello/hello.module';
 import { StoreModule } from './module/store/store.module';
 
 @Module({
@@ -12,7 +11,6 @@ import { StoreModule } from './module/store/store.module';
         }),
         PrismaModule,
         AuthModule,
-        HelloModule,
         StoreModule,
     ],
 })
